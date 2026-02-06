622e0e587e278061cba9351ce2b5612b1ccf66b5
prachi savner <prachisavner24@gmail.com>
2026-02-06 14:16:35 +0530

    dashboard

--- src/landing_page/support/SupportPage.js
+++ src/landing_page/support/SupportPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from './Hero';
+import CreateTicket from './CreateTicket';
 function SupportPage() {
     return (
-        <div className="support-container">
-            <h1>Support Page</h1>
-        </div>
+        <>
+            <Hero/>
+            <CreateTicket/>
+        </>
     );
 }
 
